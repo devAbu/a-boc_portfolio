42ef730dc1898267789795b22a3af3e5912c922b
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-10-13 17:32:59 +0200

    juhu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if (screen && screen.width <= 600) {
-    document.getElementsByTagName("circle")[0].setAttribute("r", "150");
-    document.getElementsByClassName("circle")[0].style.marginTop = "10%";
+  document.getElementsByTagName("circle")[0].setAttribute("r", "100");
+  document.getElementsByClassName("circle")[0].style.marginTop = "10%";
 } else if (screen && screen.width <= 769) {
-    document.getElementsByTagName("circle")[0].setAttribute("r", "150");
+  document.getElementsByTagName("circle")[0].setAttribute("r", "150");
 }
