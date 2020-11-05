90eb5367192c98613a1fa4deee6e13e0bf349e98
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2020-11-05 19:49:51 +0100

    Send email

---
+++ sendEmail.php
@@ -0,0 +1,31 @@
+<?php
+
+$email = "info@a-boc.com";
+$userName = $_REQUEST["userName"];
+$userEmail = $_REQUEST["userEmail"];
+$userPhone = $_REQUEST["userPhone"];
+$userMessage = $_REQUEST["userMessage"];
+
+
+if ($_REQUEST['task'] == "message") {
+
+    $admin_email = "abdulrahman.almonajed@gmail.com";
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: " . $email;
+
+    $message = 'E-mail je poslao: ' . $userName . ' <br> Email adresa: ' . $userEmail . ' <br> Broj mob: ' . $userPhone . ' <br> Poruka: ' . $userMessage;
+
+    $htmlMessage = html_entity_decode($message);
+
+    //send email
+
+    if (mail($admin_email, "$userEmail", $htmlMessage, $headers)) {
+        echo ('sent');
+    } else {
+        echo ('ne radi');
+    }
+}
+
+?>
